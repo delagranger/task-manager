a4810f3c65b9de9f8ed895e2f4768d1e06810370
Auguste <longwonderstaff@gmail.com>
2026-07-03 17:25:00 +0300

    feat: add nice tasks and groups display with html

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from api.routes.tasks import router as tasks_router
 from api.routes.groups import router as groups_router
+from api.routes.pages import router as pages_router
 from config import log_setup
 
 log_setup()
@@ -10,4 +11,5 @@ app = FastAPI()
 
 app.include_router(tasks_router, prefix="/api/v1")
 app.include_router(groups_router, prefix="/api/v1")
+app.include_router(pages_router)
     
