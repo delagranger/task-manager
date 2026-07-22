1fbd20b5985f4469c3f3e686cd251eb25de19ba8
Auguste <longwonderstaff@gmail.com>
2026-07-22 19:16:21 +0300

    feat: return cli

---
+++ cli/__init__.py
@@ -0,0 +1,9 @@
+from .app import App
+from .cli_argparser import CLIArgParser
+from .cli_output import CLIOutput
+
+__all__ = [
+    "App",
+    "CLIArgParser",
+    "CLIOutput",
+]
---
+++ main_cli.py
@@ -0,0 +1,8 @@
+from config import log_setup
+
+from cli import App
+
+log_setup()
+
+if __name__ == "__main__": 
+    App().run()
