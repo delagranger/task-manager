42167db6cd5257e39c8522af20ab2484ce1cd06d
Auguste <longwonderstaff@gmail.com>
2026-07-30 22:53:55 +0300

    upd: cut exception_handler

--- api/handlers/exception_handler.py
+++ api/handlers/exception_handler.py
@@ -4,33 +4,48 @@ from fastapi.responses import JSONResponse
 from exceptions.exceptions import StatusNotFound, GIDNotFound, TIDNotFound, GroupNotFound, GroupAlreadyExists, SortTypeNotFound, FilterNotExists, IncorrectLength
 
 
-async def group_id_not_found_handler(request: Request, exc: GIDNotFound) -> JSONResponse:
-    return JSONResponse(status_code=404, content={"detail": str(exc)})
+STATUS_CODES = {
+    GIDNotFound: 404,
+    TIDNotFound: 404,
+    GroupNotFound: 404,
+    GroupAlreadyExists: 404,
+    StatusNotFound: 400,
+    SortTypeNotFound: 400,
+    FilterNotExists: 400,
+    IncorrectLength: 400
+}
 
 
-async def task_id_not_found_handler(request: Request, exc: TIDNotFound) -> JSONResponse:
-    return JSONResponse(status_code=404, content={"detail": str(exc)})
+async def validation_error_handler(request: Request, exc: Exception):
+    return JSONResponse(status_code=STATUS_CODES[type(exc)], content={"detail": str(exc)})
+
+# async def group_id_not_found_handler(request: Request, exc: GIDNotFound) -> JSONResponse:
+#     return JSONResponse(status_code=404, content={"detail": str(exc)})
 
 
-async def group_not_found_handler(request: Request, exc: GroupNotFound) -> JSONResponse:
-    return JSONResponse(status_code=404, content={"detail": str(exc)})
+# async def task_id_not_found_handler(request: Request, exc: TIDNotFound) -> JSONResponse:
+#     return JSONResponse(status_code=404, content={"detail": str(exc)})
 
 
-async def group_already_exists(request: Request, exc: GroupAlreadyExists) -> JSONResponse:
-    return JSONResponse(status_code=404, content={"detail": str(exc)})
+# async def group_not_found_handler(request: Request, exc: GroupNotFound) -> JSONResponse:
+#     return JSONResponse(status_code=404, content={"detail": str(exc)})
 
 
-async def status_not_found_handler(request: Request, exc: StatusNotFound) -> JSONResponse:
-    return JSONResponse(status_code=400, content={"detail": str(exc)})
+# async def group_already_exists(request: Request, exc: GroupAlreadyExists) -> JSONResponse:
+#     return JSONResponse(status_code=404, content={"detail": str(exc)})
 
 
-async def sort_type_not_found_handler(request: Request, exc: SortTypeNotFound) -> JSONResponse:
-    return JSONResponse(status_code=400, content={"detail": str(exc)})
+# async def status_not_found_handler(request: Request, exc: StatusNotFound) -> JSONResponse:
+#     return JSONResponse(status_code=400, content={"detail": str(exc)})
 
 
-async def filter_not_exists_handler(request: Request, exc: FilterNotExists) -> JSONResponse:
-    return JSONResponse(status_code=400, content={"detail": str(exc)})
+# async def sort_type_not_found_handler(request: Request, exc: SortTypeNotFound) -> JSONResponse:
+#     return JSONResponse(status_code=400, content={"detail": str(exc)})
 
 
-async def incorrect_length_handler(request: Request, exc: IncorrectLength) -> JSONResponse:
-    return JSONResponse(status_code=400, content={"detail": str(exc)})
+# async def filter_not_exists_handler(request: Request, exc: FilterNotExists) -> JSONResponse:
+#     return JSONResponse(status_code=400, content={"detail": str(exc)})
+
+
+# async def incorrect_length_handler(request: Request, exc: IncorrectLength) -> JSONResponse:
+#     return JSONResponse(status_code=400, content={"detail": str(exc)})
--- main_web.py
+++ main_web.py
@@ -4,15 +4,7 @@ from api.routes.tasks import router as tasks_router
 from api.routes.groups import router as groups_router
 from api.routes.pages import router as pages_router
 
-from api.handlers.exception_handler import (group_id_not_found_handler, 
-                                            task_id_not_found_handler, 
-                                            group_not_found_handler, 
-                                            group_already_exists,
-                                            status_not_found_handler, 
-                                            sort_type_not_found_handler, 
-                                            filter_not_exists_handler, 
-                                            incorrect_length_handler
-                                            )
+from api.handlers.exception_handler import validation_error_handler
 
 from exceptions.exceptions import (StatusNotFound, GIDNotFound, TIDNotFound, 
                                    GroupNotFound, GroupAlreadyExists, 
@@ -43,14 +35,14 @@ API разработан на FastAPI с использованием SQLAlchemy
 """
 )
 
-app.add_exception_handler(GIDNotFound, group_id_not_found_handler)
-app.add_exception_handler(TIDNotFound, task_id_not_found_handler)
-app.add_exception_handler(GroupNotFound, group_not_found_handler)
-app.add_exception_handler(GroupAlreadyExists, group_already_exists)
-app.add_exception_handler(StatusNotFound, status_not_found_handler)
-app.add_exception_handler(SortTypeNotFound, sort_type_not_found_handler)
-app.add_exception_handler(FilterNotExists, filter_not_exists_handler)
-app.add_exception_handler(IncorrectLength, incorrect_length_handler)
+app.add_exception_handler(GIDNotFound, validation_error_handler)
+app.add_exception_handler(TIDNotFound, validation_error_handler)
+app.add_exception_handler(GroupNotFound, validation_error_handler)
+app.add_exception_handler(GroupAlreadyExists, validation_error_handler)
+app.add_exception_handler(StatusNotFound, validation_error_handler)
+app.add_exception_handler(SortTypeNotFound, validation_error_handler)
+app.add_exception_handler(FilterNotExists, validation_error_handler)
+app.add_exception_handler(IncorrectLength, validation_error_handler)
 
 app.include_router(tasks_router, prefix="/api/v1")
 app.include_router(groups_router, prefix="/api/v1")
